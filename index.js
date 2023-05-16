9a6dc80e76bdfd5fa743e658c34b9af25abd4763
NazmulSuzon <nazmulsuzon100@gmail.com>
2023-05-16 22:56:03 +0600

    create add doctor route in dashboard

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
@@ -65,6 +65,12 @@ async function run() {
       res.send(options);
   });
 
+  app.get('/appointmentSpecialty', async(req, res) =>{
+    const query = {};
+    const result = await appointmentOptionCollection.find(query).project({name: 1}).toArray();
+    res.send(result);
+  })
+
     /*
       *API Naming Convention
       *bookings
@@ -126,9 +132,37 @@ async function run() {
       res.send(users);
     })
 
+    app.get('/users/admin/:email', async(req, res) =>{
+      const email = req.params.email;
+      const query = { email}
+      const user = await usersCollection.findOne(query);
+      res.send({isAdmin: user?.role === 'admin'});
+    })
+
     app.post('/users', async(req, res) =>{
       const user = req.body;
       const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.put('/users/admin/:id', verifyJWT, async(req, res) => {
+      const decodedEmail = req.decoded.email;
+      const query = {email: decodedEmail};
+      const user = await usersCollection.findOne(query);
+
+      if(user?.role !== admin){
+        return res.status(403).send({message: 'forbidden access'})
+      }
+
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const options = { upsert: true};
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updatedDoc, options);
       res.send(result);
     })
 
